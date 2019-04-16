177c11a5c386c47621edabd9cca32c8306d1188d
Zatchs <martin.j@simplon-charleville.fr>
2019-04-16 14:38:27 +0200

    Suppression de 4 onglets

--- sport_website/footer.php
+++ sport_website/footer.php
@@ -16,11 +16,7 @@
         <div class="footer2">
             <div class="social">&#62220;</div>
             <div class="social">&#62217;</div>
-            <div class="social">&#62223;</div>
-            <div class="social">&#62232;</div>
             <div class="social">&#62235;</div>
-            <div class="social">&#62226;</div>
-            <div class="social">&#62214;</div>
           </div>
         </div>
       </footer>
