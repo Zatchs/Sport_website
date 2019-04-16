28374684ef02731a11ed8c852315fd90e4b06765
Zatchs <martin.j@simplon-charleville.fr>
2019-04-16 14:06:45 +0200

    Ajout des classes basiques footer

--- sport_website/footer.php
+++ sport_website/footer.php
@@ -1,15 +1,25 @@
 <!DOCTYPE HTML>
 <html>
     <head>
-
-        <meta charset="utf-8"/>
-        <link rel="stylesheet" href="style_footer.css"/>
-
+        <meta charset="utf-8">
+      	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+      	<meta name="viewport" content="width=device-width, initial-scale=1">
+      	<link rel="stylesheet" href="css/footer.css">
 
     </head>
-
-
     <body>
+      <footer>
+        <div class="footer1">
+        </div>
+
+        <div class="footer2">
+        </div>
+
+
+
+
+
+
     </body>
 
 </html>
