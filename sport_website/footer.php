a7bd63d9718a09b1cd941a9290211b3f9ee314cd
Zatchs <martin.j@simplon-charleville.fr>
2019-04-16 14:04:48 +0200

    Ajout du footer + css (vide)

---
+++ sport_website/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style_footer.css"/>
+
+
+    </head>
+
+
+    <body>
+    </body>
+
+</html>
