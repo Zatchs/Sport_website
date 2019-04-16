a4614790786b9fdf405a795d92ed44cb59278c74
Zatchs <martin.j@simplon-charleville.fr>
2019-04-16 14:21:55 +0200

    Idée pour footer 2

--- sport_website/footer.php
+++ sport_website/footer.php
@@ -4,16 +4,31 @@
         <meta charset="utf-8">
       	<meta http-equiv="X-UA-Compatible" content="IE=edge">
       	<meta name="viewport" content="width=device-width, initial-scale=1">
-      	<link rel="stylesheet" href="css/footer.css">
+      	<link rel="stylesheet" href="css/style_footer.css">
 
     </head>
     <body>
       <footer>
         <div class="footer1">
+
         </div>
 
         <div class="footer2">
+          <div class="content">Contactez-nous</div>
+          <div class="footer">
+            <div class="social">&#62220;</div>
+            <div class="social">&#62217;</div>
+            <div class="social">&#62223;</div>
+            <div class="social">&#62232;</div>
+            <div class="social">&#62235;</div>
+            <div class="social">&#62226;</div>
+            <div class="social">&#62214;</div>
+          </div>
         </div>
+      </footer>
+    </body>
+    </html>
+
 
 
 
