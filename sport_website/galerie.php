09b25c408c4781ea7b94206ff0dcfa6be0d87dc3
Zatchs <martin.j@simplon-charleville.fr>
2019-04-17 15:55:25 +0200

    Ajout des photos et ajout d'une page simple pour galerie

--- sport_website/galerie.php
+++ sport_website/galerie.php
@@ -5,52 +5,53 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/galerie.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
 
 </head>
 
 <body>
+
+	<?php include('header.php'); ?>
+	<h2>Galerie photo</h2>
 	<div class="ligne1">
 			<div class="photol1">
 				<ul id="photol1">
 					<li>
-						<img src="img/1.jpg" alt="Tome 1"></a>
+						<img src="images/1.jpg" alt=""></a>
 					</li>
 					<li>
-						<img src="img/2.jpg" alt="Tome 2"></a>
+						<img src="images/2.jpg" alt=""></a>
 					</li>
 					<li>
-						<img src="img/3.jpg" alt="Tome 3"></a>
+						<img src="images/3.jpg" alt=""></a>
 					</li>
 					<li>
-						<img src="img/4.jpg" alt="Tome 4"></a>
+						<img src="images/4.jpg" alt=""></a>
 					</li>
 				</ul>
 			</div>
 		</div>
-		<br>
-		<br>
 		<div class="ligne2">
 			<div class="photol1">
 				<ul id="photol1">
 					<li>
-						<img src="img/5.jpg" alt="Tome 5"></a>
+						<img src="images/5.jpg" alt=""></a>
 					</li>
 					<li>
-					<img src="img/6.jpg" alt="Tome 6"></a>
+					<img src="images/6.jpg" alt=""></a>
 					</li>
 					<li>
-						<img src="img/7.jpg" alt="Tome 7"></a>
+						<img src="images/7.jpg" alt=""></a>
 					</li>
 					<li>
-						<img src="img/8.jpg" alt="Tome 8"></a>
+						<img src="images/8.jpg" alt=""></a>
 					</li>
 				</ul>
 			</div>
 		</div>
-
+<?php include('footer.php'); ?>
 </body>
 
 </html>
