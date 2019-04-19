c6fbdc999a50c3888d6bb4d81170133e052fc5d8
1 <g.ponsard@simplon-charleville.fr>
2019-04-19 16:30:39 +0200

    début mise en forme du formulaire

--- sport_website/contact.php
+++ sport_website/contact.php
@@ -10,32 +10,62 @@
 </head>
 
 <body>
-<?php include('header.php'); ?>
+    <!--------------------------------------------------------------------------------
+    ------------------------------- HEADER CONTACT -----------------------------------
+    --------------------------------------------------------------------------------->
+
+    <?php include('header.php'); ?>
     <div id="header_contact">
         <div id="cadre_titre">
             <h1>CONTACT</h1>
         </div>
     </div>
-    <p>Veuillez remplir le formulaire ci-dessous pour prendre contact.
-    </p>
+    <!---FIN <div id="header_contact"> --->
 
-    <form method="post" action="traitement_formulaire.php">
-        <fieldset>
 
-            <legend>Vos coordonnées</legend>
-            <input type="text" name="nom" id="nom" placeholder="Nom" size="30" maxlength="20" />
-            <br />
-            <input type="email" name="email" id="email" placeholder="Email" size="30" maxlength="20" />
-            <br />
-            <input type="text" name="sujet" id="sujet" placeholder="Sujet" size="30" maxlength="20" />
-        </fieldset>
-        <fieldset>
 
-            <legend>Votre message</legend>
-            <textarea name="message" id="message" rows="10" cols="50" placeholder="Texte"></textarea>
-        </fieldset>
-        <input type="submit" value="Envoyer" />
-    </form>
+    <!-------------------------------------------------------------------------------
+    -----------------------------FORMULAIRE CONTACT----------------------------------
+    -------------------------------------------------------------------------------->
+
+
+    <div id="formulaire">
+
+        <!------FORMULAIRE GAUCHE -------->
+        <div class="box_form">
+            <form method="post" action="traitement_formulaire.php">
+                <fieldset>
+                    <legend>Vos coordonnées</legend>
+                    <input type="text" name="nom" id="nom" placeholder="Nom" size="30" maxlength="20" />
+                    <br />
+                    <input type="email" name="email" id="email" placeholder="Email" size="30" maxlength="20" />
+                    <br />
+                    <input type="text" name="sujet" id="sujet" placeholder="Sujet" size="30" maxlength="20" />
+                </fieldset>
+        </div>
+        <!---FIN div class="formulaire_gauche" --->
+
+
+        <!------FORMULAIRE DROITE--------->
+        <div class="box_form">
+            <fieldset>
+                <legend>Votre message</legend>
+                <textarea name="message" id="message" rows="10" cols="50" placeholder="Texte"></textarea>
+            </fieldset>
+        </div>
+        <!---FIN <div class="fomulaire_droite">--->
+
+
+        <!-------BOUTON ENVOYER---------->
+        <div id=bouton_form>
+            <input type="submit" value="Envoyer" />
+        </div>
+        </form>
+        <!---FIN <form method="post" action="traitement_formulaire.php"> --->
+
+    </div>
+    <!---FIN <div id="formulaire"> --->
+
     <?php include('footer1.php'); ?>
     <?php include('footer2.php'); ?>
 </body>
