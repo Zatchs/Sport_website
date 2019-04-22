c563887244f1400bc7ed69e5d1aaea56ca0a49dc
gaelponsard <g.ponsard@simplon-charleville.fr>
2019-04-22 14:21:17 +0200

    ajout map et join us

--- sport_website/contact.php
+++ sport_website/contact.php
@@ -38,26 +38,26 @@
         <div id="conteneur-form">
             <!------FORMULAIRE PARTIE GAUCHE -------->
             <div class="element-form1">
-                
-                    <legend>Vos coordonnées</legend>
-                    <input type="text" name="nom" id="nom" placeholder="Nom" size="40" maxlength="20" />
-                    <br />
-                    <input type="text" name="prenom" id="prenom" placeholder="Prénom" size="40" maxlength="20" />
-                    <br />
-                    <input type="email" name="email" id="email" placeholder="Email" size="40" maxlength="20" />
-                    <br />
-                    <input type="text" name="sujet" id="sujet" placeholder="Sujet" size="40" maxlength="20" />
-                
+
+                <legend>Vos coordonnées</legend>
+                <input type="text" name="nom" id="nom" placeholder="Nom" size="40" maxlength="20" />
+                <br />
+                <input type="text" name="prenom" id="prenom" placeholder="Prénom" size="40" maxlength="20" />
+                <br />
+                <input type="email" name="email" id="email" placeholder="Email" size="40" maxlength="20" />
+                <br />
+                <input type="text" name="sujet" id="sujet" placeholder="Sujet" size="40" maxlength="20" />
+
             </div>
             <!------FIN FORMULAIRE PARTIE GAUCHE------>
 
 
             <!-------FORMULAIRE PARTIE DROITE--------->
             <div class="element-form2">
-                
-                    <legend>Votre message</legend>
-                    <textarea name="message" id="message" rows="16" cols="40" placeholder="Texte"></textarea>
-                
+
+                <legend>Votre message</legend>
+                <textarea name="message" id="message" rows="16" cols="40" placeholder="Texte"></textarea>
+
             </div>
             <!---FIN FORMULAIRE PARTIE DROITE--->
 
@@ -73,13 +73,34 @@
     </form>
 
     <!-- PARRALAX -->
-	<div class="parallax-window" data-parallax="scroll" data-image-src="images/sport2.jpg" alt=""></div>
+    <div class="parallax-window" data-parallax="scroll" data-image-src="images/sport1.jpg" alt=""></div>
     <!-- FIN PARRALAX -->
-    
+
 
     <!------ MAP ------>
 
-    <div id=map></div>
+    <div id=map><iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2576.956194069737!2d4.720275316032564!3d49.76808097938698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ea0e062e03d7e9%3A0xc5cc5f6ee91f2b25!2sCCI+des+Ardennes!5e0!3m2!1sfr!2sfr!4v1555930070253!5m2!1sfr!2sfr"
+            frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+
+
+    <!----- JOIN US ----->
+
+    <div id="join_us">
+        <h2>Rejoignez-nous !</h2><br>
+        <h3>Adhésion</h3><br>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis repellat nisi saepe ducimus maxime magnam
+            unde, ea aspernatur rem veniam quibusdam eaque. Aut, ullam fugiat necessitatibus quas laudantium fugit
+            molestiae!</p><br>
+        <h4>Quels sont les avantages à devenir membre?</h4><br>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit labore beatae ipsa voluptatum nisi
+            possimus quaerat? Laboriosam reprehenderit eum ipsa perspiciatis quibusdam, dicta animi error, natus
+            enim non repellendus quidem!</p><br>
+        <h4>Comment nous rejoindre?</h4><br>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore unde cupiditate ipsum dolore? Nihil,
+            exercitationem impedit.</p><br>
+    </div>
 
 
 
@@ -87,7 +108,7 @@
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
     <?php include('footer1.php'); ?>
     <?php include('footer2.php'); ?>
 </body>
