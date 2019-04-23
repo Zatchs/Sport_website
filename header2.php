5fcbadea5d3b44eb3cc7b59a7027c3145b6065a8
amandine1799 <a.gaignere@simplon-charleville.fr>
2019-04-23 11:14:18 +0200

    nouveau header pour les autres pages

---
+++ header2.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="css/header2.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+
+<header>
+<nav>
+    <ul>
+        <li class="menu"><a href="index.php">ACCUEIL</a></li>
+        <li class="menu"><a href="rank.php">CLASSEMENTS</a></li>
+        <li><a href="#"><img id="logo" src="images/logo-header.png" alt="logo"/></a></li>
+        <li class="menu"><a href="galerie.php">GALERIE</a></li>
+        <li class="menu"><a href="contact.php">CONTACT</a></li>
+    </ul>
+</nav>
+</header>
